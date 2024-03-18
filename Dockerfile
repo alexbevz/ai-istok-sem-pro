ae6c0a5cc09aafb39953aff85109060637341c8c
MirrorAzure <viaznikov00@mail.ru>
2024-03-18 09:34:22 +0300

    docker(feature): Fix docker-related files
    Changed env vars in docker-compose
    Added pipenv commands to docker-entrypoint
    Added dependecies to proximity service

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
 FROM savyaznikov/cuda12.1-cudnn8-python3.11:latest
+
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
 set -e
+
+exec pipenv shell -c "alembic upgrade head"
 
 case "$1" in
     api)
-        exec bash -c "uvicorn main:app --host 0.0.0.0 --port 8000 --reload --reload-dir app"
+        exec pipenv shell -c "uvicorn main:app --host 0.0.0.0 --port 8000 --reload --reload-dir app"
         ;;
         exec "$@"
 esac
