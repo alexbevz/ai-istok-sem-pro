2f3f0cd5f4695e1fac20b4adf698e7858b506774
MirrorAzure <viaznikov00@mail.ru>
2024-04-03 10:12:07 +0300

    api(dev): Make docs standalone

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from src.semantic_proximity.router import spsRouter
 
 from src.handler import exception_handlers
 
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi.staticfiles import StaticFiles
 
 description = """
 API для поиска семантической близости между текстами.
@@ -46,6 +52,30 @@ app = FastAPI(root_path='/api/v0',
                   "url": "https://istokmw.ru/",
               },
               openapi_tags=tags_metadata, )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
 
 app.include_router(router=roleRouter)
 app.include_router(router=userRouter)
