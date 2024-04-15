2ef650ac9c3ea4a98f14161a71afa3ac4e9bf936
MirrorAzure <viaznikov00@mail.ru>
2024-04-15 14:45:46 +0300

    docs(proximity): Add missing docstrings for schemes

--- src/semantic_proximity/scheme.py
+++ src/semantic_proximity/scheme.py
@@ -2,74 +2,75 @@ from src.scheme import BaseScheme
 from typing import Optional
 
 class BaseDataCollectionScheme(BaseScheme):
-    user_id: int
-    """ID пользователя"""
-    name: str
-    """Имя коллекции"""
-    qdrant_table_name: str
-    """Имя таблицы в qdrant"""
+    """Базовая схема коллекции"""
+    user_id: int                                # ID пользователя
+    name: str                                   # Имя коллекции
+    qdrant_table_name: str                      # Имя таблицы в qdrant
 
 class ModelDataCollectionScheme(BaseDataCollectionScheme):
-    id: int
-    """ID коллекции"""
+    """Схема модели коллекции"""
+    id: int                                     # ID коллекции
 
 class BaseCollectionItemScheme(BaseScheme):
-    data_collection_id: int
-    """Какой коллекции принадлежит элемент"""
-    content: str
-    """Содержимое элемента"""
-    user_content_id: Optional[str] = None
-    """ID элемента пользователя"""
+    """Базовая схема элемента коллекции"""
+    data_collection_id: int                     # ID коллекции, в которой находится элемент
+    content: str                                # Текст элемента
+    user_content_id: Optional[str] = None       # Пользовательский ID
 
 class ModelCollectionItemScheme(BaseCollectionItemScheme):
-    id: int
-    """ID Элемента коллекции"""
+    """Схема модели элемента коллекции"""
+    id: int                                     # ID Элемента коллекции
 
 class TextItemScheme(BaseScheme):
-    content: str
-    """Текст элемента"""
-    user_content_id: Optional[str] = None
-    """ID элемента пользователя кастомный"""
+    """Схема текстового элемента"""
+    content: str                                # Текст элемента
+    user_content_id: Optional[str] = None       # Пользовательский ID
 
 class TextProximityItemScheme(TextItemScheme):
-    semantic_proximity: float
-    """Точность сравнения"""
+    """Схема элемента со значением близости"""
+    semantic_proximity: float                   # Точность сравнения
 
 class ProximityItemsScheme(BaseScheme):
-    content: str
-    user_content_id: Optional[str] = None
-    compared_items: list[TextItemScheme]
+    """Сравниваемый элемент и список для сравнения"""
+    content: str                                # Содержимое элемента
+    user_content_id: Optional[str] = None       # Пользовательский ID
+    compared_items: list[TextItemScheme]        # Список элементов для сравнения
 
 class ProximityResultScheme(BaseScheme):
-    content: str
-    user_content_id: Optional[str] = None
-    compared_items_result: list[TextProximityItemScheme]
+    """Сравниваемый элемент и результаты сравнения"""
+    content: str                                            # Содержимое элемента
+    user_content_id: Optional[str] = None                   # Пользовательский ID
+    compared_items_result: list[TextProximityItemScheme]    # Список элементов вместе с близостью
 
 class GetProximeItemsScheme(BaseScheme):
-    count: int = 5
-    limit_accuracy: float = 0.5
+    """Настройки получения результатов сравнения"""
+    count: int = 5                              # Количество выводимых элементов
+    limit_accuracy: float = 0.5                 # Предел точности
 
 class SaveProximeItemsScheme(GetProximeItemsScheme):
-    save: bool = False
+    """Сохранение сравниваемого элемента в коллецию"""
+    save: bool = False                          # Флаг для сохранения
 
 class CreateDataCollectionScheme(BaseScheme):
-    name: str
+    """Схема для создания коллекции"""
+    name: str                                   # Имя коллекции
 
 class UpdateDataCollectionScheme(CreateDataCollectionScheme):
+    """Схема для редактирования коллекции"""
     pass
 
 class GetDataCollectionScheme(BaseScheme):
-    id: int
-    name: str
-
-class CreateItemResponseScheme(ModelCollectionItemScheme):
-    pass
+    """Схема для получения коллекции по id"""
+    id: int                                     # ID коллекции
+    name: str                                   # Имя коллекции
 
 class GetAllCollectionElementsScheme(BaseScheme):
-    result: list[ModelCollectionItemScheme]
-    offset: int
-    limit: int
-    total: int
+    """Схема всех элементов коллекции с пагинацией"""
+    result: list[ModelCollectionItemScheme]     # Список полученных элементов
+    offset: int                                 # Отступ
+    limit: int                                  # Лимит
+    total: int                                  # Количество полученных элементов
 
 class NumberOfCreatedItemsScheme(BaseScheme):
-    total: int
+    """Количество добавленных элементов"""
+    total: int                                  # Количество элементов
