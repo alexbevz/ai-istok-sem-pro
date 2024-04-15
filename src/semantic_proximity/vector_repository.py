c90d755cfcb2f3d61ac9943d84304b8983d5e8b6
MirrorAzure <viaznikov00@mail.ru>
2024-04-15 11:32:05 +0300

    refactor(proximity): Move fields to separate variables

--- src/semantic_proximity/vector_repository.py
+++ src/semantic_proximity/vector_repository.py
@@ -1,35 +1,39 @@
-from typing import Any, List, Optional
+from typing import Any, List
 
 from qdrant_client.models import PointStruct, PointIdsList, Record, InitFrom
 
 from src.semantic_proximity.vector_database import QdrantClientManager
 from src.semantic_proximity.config import QdrantConfig
+from src.semantic_proximity.exception import QdrantCollectionException
 
 class VectorRepository:
 
     @classmethod
     def create_collection(cls, collection_name: str) -> str:
         with QdrantClientManager() as client:
+            vector_config = QdrantConfig.get_vector_config()
             success = client.create_collection(collection_name=collection_name,
-                                     vectors_config=QdrantConfig.get_vector_config())
-            if success:
-                return collection_name
-            else:
-                return None
+                                     vectors_config=vector_config)
+            if not success:
+                raise QdrantCollectionException(f"Could not create collection {collection_name}")
+            return collection_name
     
     @classmethod
     def recreate_collection(cls, collection_name: str) -> str:
         with QdrantClientManager() as client:
+            vector_congif = QdrantConfig.get_vector_config()
             client.recreate_collection(collection_name=collection_name,
-                                       vectors_config=QdrantConfig.get_vector_config())
+                                       vectors_config=vector_congif)
         return collection_name
     
     @classmethod
     def create_from_collection(cls, collection_name: str, base_collection: str) -> str:
         with QdrantClientManager() as client:
+            init_collection = InitFrom(collection=base_collection)
+            vector_config = QdrantConfig.get_vector_config()
             client.create_collection(collection_name=collection_name,
-                                     vectors_config=QdrantConfig.get_vector_config(),
-                                     init_from=InitFrom(collection=base_collection))
+                                     vectors_config=vector_config,
+                                     init_from=init_collection)
         return collection_name
 
     @classmethod
@@ -45,9 +49,7 @@ class VectorRepository:
 
     @classmethod
     def add_point(cls, collection_name: str, point: PointStruct):
-        with QdrantClientManager() as client:
-            client.upsert(collection_name=collection_name,
-                          points=[point])
+        cls.add_points(collection_name, [point])
     
     @classmethod
     def add_points(cls, collection_name: str, points: list[Any]):
@@ -57,11 +59,7 @@ class VectorRepository:
 
     @classmethod
     def get_point_by_id(cls, collection_name: str, point_id: int) -> List[Record]:
-        with QdrantClientManager() as client:
-            return client.retrieve(collection_name=collection_name,
-                                   ids=[point_id],
-                                   with_payload=True,
-                                   with_vectors=True)
+        return cls.get_points_by_ids(collection_name, [point_id])
     
     @classmethod
     def get_points_by_ids(cls, collection_name: str, point_ids: list[Any]) -> List[Record]:
@@ -78,17 +76,14 @@ class VectorRepository:
     @classmethod
     def delete_points_by_ids(cls, collection_name: str, point_ids: list[Any]):
         with QdrantClientManager() as client:
+            points = PointIdsList(points=point_ids)
             client.delete(collection_name=collection_name,
-                          points_selector=PointIdsList(
-                                 points=point_ids
-                                 )
+                          points_selector=points
                          )
     
     @classmethod
-    def find_nearest_by_vector(cls, collection_name: str, vector, limit: Optional[int]=None):
+    def find_nearest_by_vector(cls, collection_name: str, vector, limit: int):
         with QdrantClientManager() as client:
-            if limit is None:
-                limit = cls.get_collection_len(collection_name)
             return client.search(collection_name=collection_name,
                                query_vector=vector,
                                limit=limit,
